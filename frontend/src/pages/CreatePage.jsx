71afe0a825766d85696a0c8b7bed0029122a3778
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-06 00:07:12 +0545

    Update the product form

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,6 +1,6 @@
 import { useColorModeValue } from '@chakra-ui/react';
 import  { useState } from 'react'
-import { Box, Container, Heading, VStack, Input, Button } from '@chakra-ui/react';
+import { Box, Container, Heading, VStack, Input, Button, useToast } from '@chakra-ui/react';
 import { useProductStore } from '../store/product';
 
 const CreatePage = () => {
@@ -9,11 +9,27 @@ const CreatePage = () => {
         price: '',
         image: ''
     });
+    const toast = useToast();
     const {createProduct} = useProductStore();
     const handleAddProduct = async()=>{
         const {success, message} = await createProduct(newProduct);
-        console.log("Success:", success);
-        console.log("Message:", message);
+        if(!success){
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                isClosable: true,
+            })
+        }else{
+            toast({
+                title: "Success",
+                description: message,
+                status: "success",
+                isClosable: true,
+            });
+        }
+        setNewProduct({ name: '', price: '', image: '' }); // Reset the form
+        
     }
   return (
     <Container maxW={"container.sm"}>
