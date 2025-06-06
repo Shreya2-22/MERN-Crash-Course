104b0b16905815d18758e2ad73bbfdeab0ca4da2
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-06 09:59:22 +0545

    display products

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -3,6 +3,7 @@ import {create} from "zustand";
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
+
   createProduct: async(newProduct)=>{
     if(!newProduct.name || !newProduct.price || !newProduct.image) {
       return {success: false, message: "Please fill in all fields."};
@@ -19,5 +20,25 @@ export const useProductStore = create((set) => ({
       products: [...state.products, data.data],
     }));
     return {success:true, message: "Product created successfully."};
-  }
+  },
+
+  fetchProducts: async () => {
+    const res = await fetch("/api/products");
+    const data = await res.json();
+    set({ products: data.data });
+  },
+
+  deleteProduct: async (id) => {
+    const res = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if(!data.success) {
+      return {success: false, message: data.message};
+    }
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== pid)
+    }));
+    return {success: true, message: data.message};
+  },
 }));
